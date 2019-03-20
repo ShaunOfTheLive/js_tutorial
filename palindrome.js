7a1d6779e6c2d78515532071be7b4bcbbd9e8d82
Shaun Hill <shaun.hill@gmail.com>
2019-03-19 23:38:55 -0400

    starting chapter 8

--- palindrome.js
+++ palindrome.js
@@ -3,9 +3,38 @@ function reverse(string) {
   return Array.from(string).reverse().join("");
 }
 
-// Returns true for a paindrome, false otherwise.
+// Returns true for a palindrome, false otherwise.
 function palindrome(string) {
   let processedContent = string.toLowerCase();
   return processedContent === reverse(processedContent);
+}
+
+// Adds `reverse` to all strings.
+String.prototype.reverse = function() {
+  return Array.from(this).reverse().join("");
+}
+
+// Defines a Phrase object.
+function Phrase(content) {
+  this.content = content;
+
+  this.processor = function(string) {
+    return string.toLowerCase();
+  }
+
+  // Returns content processed for palindrome testing.
+  this.processedContent = function processedContent() {
+    return this.processor(this.content);
+  }
+
+  // Returns true if the phrase is a palindrome, false otherwise.
+  this.palindrome = function palindrome() {
+    return this.processedContent() === this.processedContent().reverse();
+  }
+
+  // Makes the phrase LOUDER.
+  this.louder = function() {
+    return this.content.toUpperCase();
+  }
 }
 
